7af8444fe44f6988200e5879d0fe96466cd31fef
Luan Accioly <acciolyluan@gmail.com>
2023-08-11 12:09:48 -0300

    feat: add 'Fail Login' test

--- cypress/e2e/Desafio.cy.js
+++ cypress/e2e/Desafio.cy.js
@@ -1,39 +1,15 @@
 import { Home } from "./pages/Home/Home";
 import { Signin } from "./pages/Signin/Signin";
 import { Signup } from "./pages/Signup/Signup";
+import {
+  userToRegister,
+  registeredUser,
+  userToFailLogin,
+} from "../fixtures/users";
 
 const home = new Home();
 const signin = new Signin();
 const signup = new Signup();
-
-const registeredUser = {
-  fullName: "Luan Lucas Dias Accioly",
-  email: "acciolyluan@gmail.com",
-  password: "123456",
-};
-
-const userToRegister = {
-  firstName: "Luan",
-  lastName: "Accioly",
-  fullName: "Luan Lucas Dias Accioly",
-  email: "accioly@gmail.com",
-  gender: "Mr",
-  password: "123456",
-  birth: {
-    day: "27",
-    month: "December",
-    year: "2000",
-  },
-  newsletter: true,
-  offers: true,
-  company: "TDS Company",
-  address: "Rua dos bobos, 0",
-  country: "Canada",
-  city: "Recife",
-  state: "Pernambuco",
-  zipCode: "12123-123",
-  mobileNumber: "81979145728",
-};
 
 beforeEach(() => {
   cy.visit("/");
@@ -68,11 +44,20 @@ describe("Challenges", () => {
     home.deleteAccountButton().click();
     home.deleteAccountResult().should("contain.text", "Account Deleted!");
   });
+
   it("Login with registered user", () => {
     home.signinSignupButton().click();
     signin.loginEmail().type(registeredUser.email);
     signin.loginPassword().type(registeredUser.password);
     signin.loginButton().click();
     home.loggedAs().should("contain.text", registeredUser.fullName);
+  });
+
+  it("Login with unregistered user", () => {
+    home.signinSignupButton().click();
+    signin.loginEmail().type(userToFailLogin.email);
+    signin.loginPassword().type(userToFailLogin.password);
+    signin.loginButton().click();
+    cy.contains("Your email or password is incorrect!").should("be.visible");
   });
 });
