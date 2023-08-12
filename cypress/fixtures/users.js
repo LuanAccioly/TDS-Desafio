604bda1a09b4f8dbcf91e5d1ae24af9f71f2dd9a
Luan Accioly <acciolyluan@gmail.com>
2023-08-12 03:24:00 -0300

    feat: add data to Registered User

--- cypress/fixtures/users.js
+++ cypress/fixtures/users.js
@@ -1,7 +1,31 @@
 export const registeredUser = {
+  firstName: "Luan",
+  lastName: "Accioly",
   fullName: "Luan Lucas Dias Accioly",
   email: "acciolyluan@gmail.com",
+  gender: "Mr",
   password: "123456",
+  birth: {
+    day: "27",
+    month: "December",
+    year: "2000",
+  },
+  newsletter: true,
+  offers: true,
+  company: "TDS Company",
+  address: "Rua dos bobos, 0",
+  country: "Canada",
+  city: "Recife",
+  state: "Pernambuco",
+  zipCode: "12123-123",
+  mobileNumber: "81979145728",
+  card: {
+    name: "Luan Accioly",
+    number: "1234123412341234",
+    expiryMonth: "12",
+    expiryYear: "2023",
+    cvv: "123",
+  },
 };
 
 export const userToRegister = {
